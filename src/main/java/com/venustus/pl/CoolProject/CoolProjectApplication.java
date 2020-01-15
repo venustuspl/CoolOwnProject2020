cba7c35bb65b6186e2f8e2b06a136aa60be85558
venustuspl <venustus.pl@gmail.com>
2020-01-15 19:50:08 +0100

    0_1

---
+++ src/main/java/com/venustus/pl/CoolProject/CoolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.venustus.pl.CoolProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venustus/pl/CoolProject/CoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venustus.pl.CoolProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
